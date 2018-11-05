bff5c64a8c804a5cb8b72b85699e5c583ba3f179
yangyifeng <yangyifeng@idengyun.com>
2018-11-05 18:53:30 +0800

    start

---
+++ idengyunstart/src/com/idengyun/start/ProjectStart.java
@@ -0,0 +1,11 @@
+package com.idengyun.start;
+
+public class ProjectStart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "mi";
+		System.out.println("name:"+str);
+	}
+
+}
